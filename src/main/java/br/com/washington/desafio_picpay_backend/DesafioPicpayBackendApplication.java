72b3b20beb19440991ac00075d101a4ff2edb75a
Washington <washington195luiz@gmail.com>
2024-09-04 19:57:12 -0300

    First commit

---
+++ src/main/java/br/com/washington/desafio_picpay_backend/DesafioPicpayBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.washington.desafio_picpay_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPicpayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPicpayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/washington/desafio_picpay_backend/DesafioPicpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.washington.desafio_picpay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
